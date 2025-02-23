e6b892c2a97bdff1264ac9664235c5ad79f8f92f
Jorge Ramos <jorgeivanramosmurgas@gmail.com>
2025-02-23 17:22:22 -0500

    Realizado una parte del menu

--- NotApps/Presentacion/MenuPrincipal.cs
+++ NotApps/Presentacion/MenuPrincipal.cs
@@ -8,5 +8,42 @@ namespace NotApps.Presentacion
 {
     class MenuPrincipal
     {
+        public void menu()
+        {
+            int op;
+
+            do
+            {
+                Console.WriteLine("MENU PRINCIPAL");
+                Console.WriteLine("1- AGREGAR ESTUDIANTE");
+                Console.WriteLine("2- CONSULTAR ESTUDIANTE");
+                Console.WriteLine("3- CONSULTA SELECTIVA ESTUDIANTE");
+                Console.WriteLine("4- SALIR");
+
+                Console.WriteLine("SELECCIONE UNA OPCION (1-4)");
+                op = int.Parse(Console.ReadLine());
+
+                switch (op)
+                {
+                    case 1:
+                        EstudiantePresentacion estupresentacion = new EstudiantePresentacion();
+                        Console.Clear();
+                        estupresentacion.CapturarNotas();
+                        break;
+
+                    case 2:
+                        break;
+
+                    case 3:
+                        break;
+
+                    case 4:
+                        Environment.Exit(0);
+                        break;
+                }
+
+
+            } while (op != 3);
+        }
     }
 }
